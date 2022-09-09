4046a50f1dc445199d751a228e8d778aab0c2244
Moses Onche <onchemoses38@gmail.com>
2022-09-09 21:05:30 +0100

    Double digit number generation

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main
+ * main - Entry point
  *
- * Return
+ * Return: always 0
  *
  */
 int main(void)
@@ -23,7 +23,7 @@ int main(void)
 				{
 					putchar(',');
 					putchar(' ');
-				}	
+				}
 			}
 		}
 	}
