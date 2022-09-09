c40f7d8fb397cbd92bdf42d6fe8f80304a59bb1c
Moses Onche <onchemoses38@gmail.com>
2022-09-09 15:52:01 +0100

    Alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	char alp;
+	for (alp = 'a'; alp <= 'z'; alp++)
+		putchar(alp);
+	putchar('\n');
+	return (0);
+}
