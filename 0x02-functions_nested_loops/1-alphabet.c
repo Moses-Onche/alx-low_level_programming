b7f852a9e99345e5a0d12e6af4676b9d45c6f58d
Moses Onche <onchemoses38@gmail.com>
2022-09-13 22:04:29 +0100

    Print all the alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,40 +6,19 @@
  * Return: always zero
  *
  */
+void print_alphabet(void);
+
 int main(void)
 {
-	void print_alphabet(void);
 	print_alphabet();
 	return (0);
 }
 
 void print_alphabet(void)
 {
-	_putchar('a');
-	_putchar('b');
-	_putchar('c');
-	_putchar('d');
-	_putchar('e');
-	_putchar('f');
-	_putchar('g');
-	_putchar('h');
-	_putchar('i');
-	_putchar('j');
-	_putchar('k');
-	_putchar('l');
-	_putchar('m');
-	_putchar('n');
-	_putchar('o');
-	_putchar('p');
-	_putchar('q');
-	_putchar('r');
-	_putchar('s');
-	_putchar('t');
-	_putchar('u');
-	_putchar('v');
-	_putchar('w');
-	_putchar('x');
-	_putchar('y');
-	_putchar('z');
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++);
+		_putchar(a);
 	_putchar('\n');
 }
