068c99e13f930c33835f392e4de968f1eb69b821
Moses Onche <onchemoses38@gmail.com>
2022-09-13 21:30:14 +0100

    Header file issues

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,1 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes char c to std output
- *
- * Return: on success, returns 1
- * on error, -1 is returned and errno is set appropriately
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
