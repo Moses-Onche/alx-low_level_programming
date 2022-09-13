9723d43187725b2f976cf87972bd3ecde0806b3c
Moses Onche <onchemoses38@gmail.com>
2022-09-13 23:24:11 +0100

    Lower case checker

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,9 @@
 /**
  * _islower(int c) - checks for lower case
  *
+ *@c: character to compare
  *
  * Return: _islower(int c) returns 1 if c is lowercase, returns 0 otherwise
- *
  */
 int _islower(int c)
 {
