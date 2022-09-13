6e91a38fdb2748b086b156d515efa9216e09bd1f
Moses Onche <onchemoses38@gmail.com>
2022-09-13 22:16:22 +0100

    Prototype test 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,19 +6,9 @@
  * Return: always zero
  *
  */
-void print_alphabet(void);
 
 int main(void)
 {
 	print_alphabet();
 	return (0);
-}
-
-void print_alphabet(void)
-{
-	char a;
-
-	for (a = 'a'; a <= 'z'; a++)
-		_putchar(a);
-	_putchar('\n');
 }
