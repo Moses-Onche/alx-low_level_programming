6f5c5fe02ac73231b076fd4a7aab728465471996
Moses Onche <onchemoses38@gmail.com>
2022-09-14 00:18:18 +0100

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - main function
+ *
+ * @l: input to check last digit
+ * Return: returns the last digit of l
+ *
+ */
+int print_last_digit(int l)
+{
+	return (l % 10);
+}
