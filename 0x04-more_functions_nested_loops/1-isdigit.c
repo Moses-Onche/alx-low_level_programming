14dffb72af4d86603d9965c3d1439530957deaed
Moses Onche <onchemoses38@gmail.com>
2022-09-15 19:47:54 +0100

    Is digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,13 @@
 /**
- * _isdigit - checks if digit
+ * _isdigit - determine if digit
  *
  * @c: character to determine
  *
- * Return: 1 if digit, 0 if not digit
+ * Return: gives boolean
  */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
-	{
+	if (c >= '0' && c <= '9')
 		return (1);
-	}
 	return (0);
 }
