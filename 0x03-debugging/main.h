4134f48b3c56d26c0657553d40df20832c22002f
Moses Onche <onchemoses38@gmail.com>
2022-09-16 10:23:21 +0100

    OBO Largest

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,8 +4,8 @@ i#ifndef MAIN_H
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
 int convert_day(int, int);
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
