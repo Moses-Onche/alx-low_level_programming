3c0c9c1f39bbad48328bcb738759c12012d14cb8
Moses Onche <onchemoses38@gmail.com>
2022-09-19 09:51:48 +0100

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H_   /* Include guard */
+#define MAIN_H_
+
+void reset_to_98(int *n);
+
+#endif // FOO_H_
