cf19d5f47e04a4023d67cfb83bda73599ebd9a01
Moses Onche <onchemoses38@gmail.com>
2022-09-19 11:53:58 +0100

    String to stdout in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * print_rev - print string from pointer in reverse
+ *
+ * @s: pointer char
+ * Return: always 0
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = (_strlen(s) - 1); i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
