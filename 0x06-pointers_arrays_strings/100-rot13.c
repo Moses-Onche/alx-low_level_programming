6d6b72f3c78780835c31b02f7c6a9df0c60e2535
Moses Onche <onchemoses38@gmail.com>
2022-09-23 21:49:19 +0100

    Encode to rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,15 +11,15 @@ char *rot13(char *s)
 	int i;
 	int j;
 	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLMN";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMNnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; *(s + i); i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 52; j++)
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			if (a[j] == *(s + i))
+			if (s[i] == a[j])
 			{
-				*(s + i) = b[j];
+				s[i] = b[j];
 				break;
 			}
 		}
