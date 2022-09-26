82966d976e7a366602c09b13674e8184a1a780b5
Moses Onche <onchemoses38@gmail.com>
2022-09-26 13:40:04 +0100

    Memory to array

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - Set memory to array
+ *
+ * @s: array to set
+ * @b: value to set it as
+ * @n: amount of times
+ * Return: char s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
