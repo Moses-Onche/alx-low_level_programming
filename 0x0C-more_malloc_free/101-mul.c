88ee9f2bf7f1293eab91e3708c93994eeaadac67
Moses Onche <onchemoses38@gmail.com>
2022-10-06 22:10:43 +0100

    Multiply two positive integers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- :* _print - moves a string one place to the left and prints the string
+ * _print - moves a string one place to the left and prints the string
  * @str: string to move
  * @l: size of string
  *
