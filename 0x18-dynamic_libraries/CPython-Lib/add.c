3e746cd7192878cefdd0cac54a1b8483671bc604
Moses-Onche <onchemoses38@gmail.com>
2022-12-12 14:10:03 +0100

    C functions that can be called from python

---
+++ 0x18-dynamic_libraries/CPython-Lib/add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @a: integer to be added
+ * @b: integer to be added
+ * Return: returns the addition of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x18-dynamic_libraries/CPython-Lib/mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * div - divides two integers
+ * @a: integer to be divided
+ * @b: integer to be divided
+ * Return: returns the division of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
